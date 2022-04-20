49df24f54779cd17ef8bfa30e4d7119f937f03c2
Leonardo C Araújo <ogbeni-leonardo@outlook.com>
2022-04-20 15:54:37 -0300

    Resolução do requisitos 4

--- script.js
+++ script.js
@@ -16,8 +16,32 @@ for (let index = 1; index < colors.length; index += 1) {
   colors[index].style.backgroundColor = colorGenerator();
 }
 
-/* // Criador de pixels
-function pixelGenerator(parent){
-    const newPixel = document.createElement
+// Criador de pixels
+function pixelGenerator(parent) {
+  const newPixel = document.createElement('div');
+  newPixel.className = 'pixel';
+
+  parent.appendChild(newPixel);
 }
- */
+
+// Adiciona os pixels ao elemento parent com n linhas e n colunas
+function pixelsAdd(parent, lines, columns) {
+  // Altere o tamanho do meu pixel board em relação as minhas linhas e colunas
+  pixelBoardSize(parent, columns, lines);
+  for (let line = 0; line < lines; line += 1) {
+    for (let column = 0; column < columns; column += 1) {
+      pixelGenerator(parent);
+    }
+  }
+}
+
+// Altera o tamanho o pixel-board proporcionalmente ao tamanho de blocos por linha
+function pixelBoardSize(parent, width, height) {
+  parent.style.width = width * 30 + 'px';
+  parent.style.height = height * 30 + 'px';
+}
+
+const pixelsBoard = document.getElementById('pixel-board');
+
+// Adicionando pixels ao pixel-board
+pixelsAdd(pixelsBoard, 5, 5);
